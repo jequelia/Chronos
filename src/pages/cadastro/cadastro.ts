3f318e3bc5f7ee1d935cec5da372f8e40330cca4
Haniel <Haniel@lti07.cesed.local>
2019-01-10 18:44:43 -0300

    primeiro commit

---
+++ src/pages/cadastro/cadastro.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the CadastroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-cadastro',
+  templateUrl: 'cadastro.html',
+})
+export class CadastroPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CadastroPage');
+  }
+
+}
